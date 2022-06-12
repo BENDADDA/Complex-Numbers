bd48a171bc8fe11406cfa75083514b63ec185147
BENDADDA BENAOUDA <94899619+BENDADDA@users.noreply.github.com>
2022-06-12 13:43:29 -0700

    Update Complex.cs

--- Complex_Numbers/Complex_Numbers/Complex.cs
+++ Complex_Numbers/Complex_Numbers/Complex.cs
@@ -7,14 +7,23 @@ namespace Complex_Numbers
 {
     class Complex
     {
-        public double x;
-        public double y;
-
+        public double x { get; set; }
+        public double y { get; set; }
+        public double ABS { get { return Abs(); } }
+        public double ARG { get { return Arg(); } }
+        public Complex EXP { get { return Exp(); } }
+        public Complex LOG { get { return Log(); } }
+        public Complex COS { get { return Cos(); } }
+        public Complex SIN { get { return Sin(); } }
+        public Complex TAN { get { return Sin() / Cos(); } }
+        public Complex ATAN { get { return 1 / TAN; } }
+        public Complex SQRT { get { return Sqrt(); } }
         public Complex(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
+        
         public Complex()
         { 
         }
@@ -45,7 +54,7 @@ namespace Complex_Numbers
         public static Complex operator -(double a, Complex z)
         {
 
-            return new Complex(z.x - a, z.y);
+            return new Complex(a-z.x , -z.y);
         }
 
         public static Complex operator *(Complex z, Complex w)
@@ -62,7 +71,7 @@ namespace Complex_Numbers
         }
         public static Complex operator /(Complex z, Complex w)
         {
-            double d = z.x * w.x + z.y * w.y;
+            double d = w.x * w.x + w.y * w.y;
             return new Complex((z.x * w.x + z.y * w.y)/d, (z.y * w.x - z.x * w.y)/d);
         }
         public static Complex operator /(double a, Complex z)
@@ -78,11 +87,23 @@ namespace Complex_Numbers
         {
             return new Complex(-z.x , -z.y);
         }
-        public double Abs()
+        public static Complex operator ^(Complex z, double n)
+        {
+            if (n == 0) return new Complex(1, 0);
+            if (z.x == 0 && z.y == 0) return new Complex(0, 0);
+            return (n * (z.LOG)).EXP;
+        }
+        public static Complex operator ^(Complex z, Complex n)
+        {
+            if (n.x == 0 && n.y == 0) return new Complex(1, 0);
+            if (z.x == 0 && z.y == 0) return new Complex(0, 0);
+            return (n * (z.LOG)).EXP;
+        }
+        private double Abs()
         {
             return Math.Sqrt(x * x + y * y);
         }
-        public double Arg()
+        private double Arg()
         {
             double o;
             o = Math.Asin(y / Abs());
@@ -93,28 +114,32 @@ namespace Complex_Numbers
             }
             return o;
         }
-        public Complex Pow(double n)
+        private Complex Pow(double n)
         {
+           
+            if (n == 0) return new Complex(1,0);
+            if (x == 0 && y == 0) return new Complex(0,0);
             double a,b;
             a = Math.Pow(Abs(),n) * Math.Cos(n * Arg());
             b = Math.Pow(Abs(), n) * Math.Sin(n * Arg());
             return new Complex(a, b);
         }
-        public Complex Exp()
+        
+        private Complex Exp()
         {
             double a, b;
             a = Math.Exp(x) * Math.Cos(y);
             b = Math.Exp(x) * Math.Sin(y);
             return new Complex(a, b);
         }
-        public Complex Log()
+        private Complex Log()
         {
             double a, b;
             a = Math.Log(Abs());
             b = Arg();
             return new Complex(a, b);
         }
-        public Complex Cos()
+        private Complex Cos()
         {
             Complex i = new Complex(0, 1);
             Complex e1, e2;
@@ -125,7 +150,7 @@ namespace Complex_Numbers
             return (e1 + e2) / 2;
             
         }
-        public Complex Sin()
+        private Complex Sin()
         {
             Complex i = new Complex(0, 1);
             Complex e1, e2;
@@ -136,7 +161,7 @@ namespace Complex_Numbers
             return (e1 - e2) / 2;
 
         }
-        public Complex Sqrt()
+        private Complex Sqrt()
         {
             double a = (x + Math.Sqrt(x * x + y * y)) / 2;
             double b = Math.Sqrt(a - x);
@@ -144,9 +169,27 @@ namespace Complex_Numbers
         }
         public override string ToString()
         {
-            return "(" + x + "," + y + ")";
+            
+            double a, b;
+            a = Math.Round(x,2);
+            b = Math.Round(y,2);
+            return "(" + a + "," + b + ")";
+                  
         }
+        public static Complex Parse(string str)
+        {
+            int i; string a, b = "0";
+            if (str.Contains(','))
+            {
+                i = str.IndexOf(",");
+                a = str.Substring(0, i);
+                b = str.Substring(i + 1, str.Length - a.Length - 1);
 
+            }
+            else a = str;
+
+            return new Complex(double.Parse(a), double.Parse(b));
+        }
         
     }
 }
