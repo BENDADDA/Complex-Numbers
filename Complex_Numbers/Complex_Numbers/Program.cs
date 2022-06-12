971db64d3a2fb4313d4d65d56d784d9a75805e8a
BENDADDA BENAOUDA <94899619+BENDADDA@users.noreply.github.com>
2022-06-12 13:44:19 -0700

    Update Program.cs

--- Complex_Numbers/Complex_Numbers/Program.cs
+++ Complex_Numbers/Complex_Numbers/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,20 +12,28 @@ namespace Complex_Numbers
         
         static void Main(string[] args)
         {
-            Complex z,w,q,c;
-            z = new Complex(0.5,0);
-            w = new Complex(-1, 0);           
-            Console.WriteLine(w.Sqrt());
-            Console.Write("n=");
-            int n = int.Parse(Console.ReadLine());
-            //z->z^2+c
-            c = new Complex(0.25,0);
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine(z);
-                z = z * z +c;
-            }
 
+
+            Complex a, b, c, d, x, X, D1, D2, A, B, u, v;
+            Console.WriteLine("Solve the third degree equation\nEnter the values of the following variables a,b,c,d");
+            Console.Write("a=");
+            a = Complex.Parse(Console.ReadLine());
+            Console.Write("b=");
+            b = Complex.Parse(Console.ReadLine());
+            Console.Write("c=");
+            c = Complex.Parse(Console.ReadLine());
+            Console.Write("d=");
+            d = Complex.Parse(Console.ReadLine());
+
+            D1 = (b ^ 2) - 3 * a * c; D2 = (b ^ 3) - 27 * (a ^ 2) * b;
+            A = -((b ^ 2) - 3 * a * c) / (3 * (a ^ 2));
+            B = -(D2 - 3 * D1 * b) / (27 * (a ^ 3));
+            u = (-B / 2 + ((B ^ 2) / 4 + (A ^ 3) / 27).SQRT) ^ (1.0 / 3);
+            v = (-B / 2 - ((B ^ 2) / 4 + (A ^ 3) / 27).SQRT) ^ (1.0 / 3);
+            X = u + v;
+            x = X - b / (3 * a);
+
+            Console.WriteLine(x);
             Console.ReadKey();
         }
     }
